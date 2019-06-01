78722bc5cc08d244185cace39b4f5e7fff922fee
Dzagod <hryszko.jagoda13@gmail.com>
2019-06-01 23:29:13 +0200

    set port from env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('static'));
 app.listen(port, () => console.log(`app listenening on port ${port}`))
