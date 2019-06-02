3b06d8a34d8afc8d1a6638d5f5d50bd5aae9623f
Dzagod <hryszko.jagoda13@gmail.com>
2019-06-02 16:23:32 +0200

    login user to database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
 app.listen(port, () => console.log(`app listenening on port ${port}`));
 
 MongoClient.connect('mongodb://jagoda:test123@ds263876.mlab.com:63876/heroku_244jlx1p', function(err, client) {
@@ -17,7 +19,25 @@ MongoClient.connect('mongodb://jagoda:test123@ds263876.mlab.com:63876/heroku_244
         })
 
         app.post('/login', function(req, res) {
-            res.statusCode(200).send();
+            const { email, password } = req.body;
+            
+            db.collection('user').findOne({ email }, function(err, user) {
+                if (user) {
+                    if (user.password === password) {
+                        res.status(200).json({ message: 'Zalogowano.' });
+                    } else {
+                        res.status(400).json({ message: 'Błędne hasło.' });
+                    }
+                } else {
+                    db.collection('user').insert({ email, password}, function(err, result) {
+                        console.log(`stworzono uzytkownika ${email} ${result}`);
+
+                        res.status(201).json({ message: 'Stworzono uzytkownika.' })
+                    })
+
+
+                }
+            });
         })
 
     } else {
